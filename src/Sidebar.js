ec59b6136ef6d2321b4217327659c3f39765e8e0
Waleed-Abdullah <w26waleed@gmail.com>
2022-01-18 14:30:45 +0500

    completed sideBar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -9,11 +9,11 @@ import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Button } from '@mui/material';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      {/* Twitter Icon */}
       <TwitterIcon />
 
       <SidebarOption active Icon={HomeIcon} text="Home" />
@@ -25,7 +25,9 @@ function Sidebar() {
       <SidebarOption Icon={PermIdentityIcon} text="Profile" />
       <SidebarOption Icon={MoreHorizIcon} text="More" />
 
-      {/* Button -> tweet */}
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
     </div>
   );
 }
